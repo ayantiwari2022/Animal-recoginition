1e7617f2b29e13629092856f2db2f11f5191426e
ayantiwari2022 <104711497+ayantiwari2022@users.noreply.github.com>
2022-07-17 14:08:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,41 @@
 function startClassification()
 {
 navigator.mediaDevices.getUserMedia({ audio: true});
-classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/JeBtupPF-/', modelReady);
+classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/JeBtupPF-/model.json', modelReady);
 }
 
 function modelReady(){
     console.log("Model Is Ready");
     classifier.classify(gotResults);
 }
-function gotResults(Results){
-    console.log(Results);
+function gotResults(error, results){
+    if (error) {
+        console.log(error);
+    } else {
+    console.log(results);
+    random_number_r = Math.floor(Math.random() * 255) + 1;
+    random_number_g = Math.floor(Math.random() * 255) + 1;
+    random_number_b = Math.floor(Math.random() * 255) + 1;
+
+    document.getElementById("result_label").innerHTML = 'I can hear - ' +
+    results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Accuracy - ' +
+    (results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").style.color = "rgb("
+    +random_number_r+","+random_number_g+","+random_number_r+")";
+    document.getElementById("result_confidence").style.color = "rgb("
+    +random_number_r+","+random_number_g+","+random_number_r+")";
+    
+    img = document.getElementById('img');
+
+    if (results[0].label == "Dog") {
+        img.src = 'Dog.png';
+    } else if (results[0].label == "Cat") {
+        img.src = 'Cat.png';
+    } else if (results[0].label == "Puppy") {
+        img.src = 'Puppy.png';
+    } else {
+        img.src = 'speaker-icon-11.png';
+    }
+}
 }
